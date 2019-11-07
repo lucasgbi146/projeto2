fc174b00e5a2bd10478c61fe8573249e5fee34b6
Lucas Santos <lucasgbi146@gmail.com>
2019-11-07 15:01:39 -0300

    atualizacao das rotas

--- App.py
+++ App.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'pagina inicial'
+    return render_template('index.html')
 
-if __name__= "__main__":
+@app.route('/nova-mensagem')
+def novaMensagem():
+    return_template('mensagens.html')
+
+if __name__== "__main__":
     app.run(debug=True)
