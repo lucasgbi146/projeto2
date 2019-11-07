6fca3bee708828dcfd294b52b18efcf3b1cafc12
Lucas Santos <lucasgbi146@gmail.com>
2019-11-07 16:48:06 -0300

    atualizacao do app com parte do bd

--- App.py
+++ App.py
@@ -1,9 +1,12 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
+from formulario import MensagemForm
+
 app = Flask(__name__)
 db = SQLAlchemy(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///minhasmensagens.db'
+app.config['SECRET_KEY'] = 'dfsshfehryuakbfpefeg875368jgkdfoadydgkfbdfnlshobcl'
 
 class Mensagem(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -15,11 +18,23 @@ class Mensagem(db.Model):
 
 @app.route('/')
 def index():
+
+    msg = Mensagem.query.all
+
     return render_template('index.html')
 
-@app.route('/nova-mensagem')
+@app.route('/nova-mensagem', methods=['POST', 'GET'])
 def novaMensagem():
-    return_template('mensagens.html')
+
+    form = MensagemForm()
+    
+    msg = Mensagem()
+    msg.mensagem = form.mensagem.data
+    db.session.add(msg)
+    db.session.commit()
+
+    return render_template('mensagens.html', form=form)
 
 if __name__== "__main__":
     app.run(debug=True)
+
